e6343c9cfba91dfd15447c669efdaf91e51d9a32
751441271@qq.com <751441271@qq.com>
2015-10-29 23:47:09 +0800

    Wang nian

--- app/src/main/java/net/globide/Base64.java
+++ app/src/main/java/net/globide/Base64.java
@@ -568,3 +568,34 @@ public class Base64 {
         return out;
     }
 }
+/*
+public class querya{
+	public static void main(String args[]){
+	}
+}  
+class wjFrame extends JFrame{
+	public  wjFrame(){
+		setSize(WTH,HHT);  
+	    }  
+	public static final int WTH=800;  
+  public static final int HHT=577;  
+	}  
+	
+class wjPanel extends JPanel{
+	Image im;
+	public wjPanel(){
+		im=Toolkit.getDefaultToolkit().getImage("E://java学习资料//新代码//ATM analog system//BR//查询背景.jpg");//需要注意的是如果用相对路径载入图片,则图片文件必须放在类文件所在文件夹或项目的根文件夹中,否则必须用绝对路径。  
+		}  
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);  
+	    int imWidth=im.getWidth(this);  
+	    int imHeight=im.getHeight(this); //定义图片的宽度、高度  
+	    int FWidth=getWidth();  
+	    int FHeight=getHeight();//定义窗口的宽度、高度  
+	    int x=(FWidth-imWidth)/2;  
+	    int y=(FHeight-imHeight)/2;//计算图片的坐标,使图片显示在窗口正中间  
+	    g.drawImage(im,x,y,null);//绘制图片  
+	    }  
+	} 
+
+*/
